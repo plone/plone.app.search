f725ad568b9828f12ad1d44e388165a59f789fc8
Rob Gietema <rob@fourdigits.nl>
2009-08-13 15:39:06 +0000

    Initial prototype
    svn path=/plone.app.search/trunk/; revision=28775

---
+++ plone/app/search/prototype/js/init.js
@@ -0,0 +1,14 @@
+$(function(){
+    $('.actionMenuHeader').click(function() {
+        $(this).parent().children("dd").toggle();
+        return false;
+    });
+    $('#cancel').click(function() {
+        $(this).parents('.actionMenuContent').hide();
+    });
+    $('#save').click(function() {
+        $('.portalMessage dd').html('Collection is saved at: <a href="#">/Folder1/</a>');
+        $('.portalMessage').show();
+        $(this).parents('.actionMenuContent').hide();
+    });
+});
