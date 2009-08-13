7a9cbd8c2be408aa4aa036d485cfdf821ca09195
Geir Baekholt <geir@baekholt.com>
2009-08-13 16:08:54 +0000

    prototype search parsing
    svn path=/plone.app.search/trunk/; revision=28779

--- plone/app/search/browser.py
+++ plone/app/search/browser.py
@@ -6,5 +6,20 @@ from Products.CMFCore.utils import getToolByName
 class Search(BrowserView):
     
     def results(self):
+        
+        formquery = self.request.get('query',None)
+        if not formquery: 
+            return []
+        query = {}
+        
+        
+        
+        for row in formquery:
+            v = row.get('v')
+            if v:
+                query.update({row.get('i') : v})
+        self.query = query
+        
+        
         catalog = getToolByName(self.context, 'portal_catalog')
-        return IContentListing(catalog())
+        return IContentListing(catalog(query))
