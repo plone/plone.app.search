e7d994c6a54ee446603aba901631e5c4c7f2b2a2
Geir Baekholt <geir@baekholt.com>
2009-08-14 12:57:55 +0000

    change from list to dictionary for configuration data
    svn path=/plone.app.search/trunk/; revision=28832

--- plone/app/search/config.py
+++ plone/app/search/config.py
@@ -1,32 +1,30 @@
 
 
 CRITERION={
-    'indexes': [
-        {'name': 'Creator',
-         'friendly_name': 'Creator',
-         'operators': [
-            {'name': 'is_not',
-             'friendly_name': 'Does not equal',
-             'widget': 'StringWidget',},
-            {'name': 'is',
-             'friendly_name': 'Equals',
-             'widget': 'StringWidget',},
-        ],},
-        {'name': 'modified',
-         'friendly_name': 'Modification date',
-         'operators': [
-            {'name': 'smaller_or_equal',
-             'friendly_name': 'Before',
-             'widget': 'DateWidget',},
-            {'name': 'is',
-             'friendly_name': 'On',
-             'widget': 'DateWidget',},
-            {'name': 'larger_then',
-             'friendly_name': 'After',
-             'widget': 'DateWidget',},
-            {'name': 'between',
-             'friendly_name': 'between',
-             'widget': 'DateWidget',},
-        ],},
-    ],
-}
+         'Creator':{
+            'friendly_name': 'Creator',
+            'operators': {'is_not':
+                            {'friendly_name' : 'Does not equal',
+                            'widget'        : 'StringWidget',},
+                            
+                          'is':{
+                            'friendly_name' : 'Equals',
+                            'widget'        : 'StringWidget',},
+                            },
+                            
+                            
+         'modified':{
+            'friendly_name': 'Modification date',
+            'operators': {'smaller_or_equal':
+                                {'friendly_name': 'Before',
+                                'widget': 'DateWidget',},
+                          'is':
+                                {'friendly_name': 'On',
+                            'widget': 'DateWidget',},
+                          'larger_then':
+                                {'friendly_name': 'After',
+                                 'widget': 'DateWidget',},
+                                 }
+                    }
+                }
+            }
