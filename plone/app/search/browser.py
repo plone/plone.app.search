88f63872f42dce187f9ee643d1800c074569b682
Roel Bruggink <roel+plone.org@fourdigits.nl>
2009-08-14 07:57:05 +0000

    [PLIP 9352] Added support for between criteria
    svn path=/plone.app.search/trunk/; revision=28812

--- plone/app/search/browser.py
+++ plone/app/search/browser.py
@@ -6,20 +6,41 @@ from Products.CMFCore.utils import getToolByName
 class Search(BrowserView):
     
     def results(self):
-        
-        formquery = self.request.get('query',None)
-        if not formquery: 
-            return []
-        query = {}
-        
-        
-        
-        for row in formquery:
-            v = row.get('v')
-            if v:
-                query.update({row.get('i') : v})
+        # parse query
+        query=self.parseFormquery(self.request.get('query',None))
+
         self.query = query
-        
-        
+    
+        # Get me my stuff!
         catalog = getToolByName(self.context, 'portal_catalog')
-        return IContentListing(catalog(query))
+        results=catalog(query)
+        if results:
+            return IContentListing(catalog(query))
+        return []
+        
+    def parseFormquery(self, formquery):
+        query = {}
+        for row in formquery:
+            index=row.get('i')
+            values=row.get('v')
+            criteria=row.get('c')
+
+            if not values:
+                continue
+                
+            # default behaviour
+            tmp={index:values}
+            
+            # ranges
+            if criteria =='between':
+                tmp={index:{
+                    'query':values,
+                    'range':'minmax'
+                }}
+            
+            query.update(tmp)
+        return query
+        
+        
+        
+# query.i:records=modified&query.c:records=between&query.v:records:list=2009/01/01&query.v:records:list=2009/10/01
