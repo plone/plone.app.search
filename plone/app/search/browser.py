1df8789b66fd218f4021696228e52388d65d3595
Eric Steele <esteele@plone.org>
2010-03-15 09:54:40 +0000

    Revert [29850] and [29851] so that the product installs again.
    svn path=/plone.app.search/trunk/; revision=34735

--- plone/app/search/browser.py
+++ plone/app/search/browser.py
@@ -1,6 +1,10 @@
 from Products.Five.browser import BrowserView
 from plone.app.contentlisting.interfaces import IContentListing
 from Products.CMFCore.utils import getToolByName
+from config import CRITERION, SORTABLES
+import json
+from queryparser import QueryParser
+from zope.component import queryMultiAdapter, getMultiAdapter
 from ZTUtils import make_query
 
 class Search(BrowserView):
@@ -50,5 +54,66 @@ class Search(BrowserView):
         return(
             sortoption(self.request, 'relevance'),
             sortoption(self.request, 'date (newest first)', 'Date', reverse=True),
-            sortoption(self.request, 'alphabetically', 'sortable_title'),
+            sortoption(self.request, 'aphabetically', 'sortable_title'),
         )
+
+
+class AdvancedSearch(BrowserView):
+    """ """
+    
+    # This is the advanced search that uses the query view from new-style-collections.
+    # If we end up not using this view for advanced search, it should probably be moved to the collections 
+    # package
+    
+    def __init__(self, context, request):
+        self._results = None
+        self.context = context
+        self.request = request
+
+    def getNumberOfResults(self):
+        return len(self.results())
+
+    def getFormattedNumberOfResults(self):
+        return "%d items remaining" % (len(self.results()))
+
+    def results(self):
+        if self._results is None:
+            self._results = self._queryForResults()
+        return self._results
+    
+    def _queryForResults(self, formquery=None):
+        # parse query
+        if not formquery:
+            formquery=self.request.get('query', None)
+        queryparser=QueryParser(self.context, self.request)
+        query = queryparser.parseFormquery(formquery)
+
+        if not query:
+            return IContentListing([])
+
+        # sorting
+        query['sort_on'] = getattr(self.request, 'sort_on', 'getObjPositionInParent')
+        query['sort_order'] = getattr(self.request, 'sort_order', 'ascending')
+
+        # Get me my stuff!
+        catalog = getToolByName(self.context, 'portal_catalog')
+        results = catalog(query)
+
+        if results:
+            return IContentListing(results)
+        return IContentListing([])
+
+    def printQuery(self):
+        return self.query
+
+    def getConfig(self):
+        return {'indexes':CRITERION, 'sortable_indexes': SORTABLES}
+        # we wrap this in a dictionary so we can add more configuration data 
+        # to the payload in the future. This is data that will be fetched 
+        # by a browser AJAX call
+
+    def getJSONConfig(self):
+        return json.dumps(self.getConfig())
+
+    def previewSearchResults(self):
+        return getMultiAdapter((self.context, self.request),name='previewadvancedsearchresults')()
