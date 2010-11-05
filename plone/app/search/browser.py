62727641251ddda710280b6cecd58fc28267d465
Denys Mishunov <denys.mishunov@gmail.com>
2010-11-05 20:53:34 +0000

    white-spaces
    svn path=/plone.app.search/branches/jstestdriver-integration/; revision=45497

--- plone/app/search/browser.py
+++ plone/app/search/browser.py
@@ -11,12 +11,12 @@ class Search(BrowserView):
 
     def results(self, batch=False, b_size=30):
         query = {}
-        query.update(getattr(self.request, 'form',{}))
+        query.update(getattr(self.request, 'form', {}))
         if not query:
             return IContentListing([])
         catalog = getToolByName(self.context, 'portal_catalog')
         results = catalog(query)
-        
+
         if batch:
             from Products.CMFPlone import Batch
             b_start = self.request.get('b_start', 0)
@@ -24,13 +24,14 @@ class Search(BrowserView):
             return IContentListing(batch)
         return IContentListing(results)
 
-
     def getSortOptions(self):
-        """ options for sorting in the search result template, also for marking selected """
+        """ options for sorting in the search result template, also for marking
+        selected """
         q = {}
         q.update(self.request.form)
-        
+
         class sortoption(object):
+
             def __init__(self, request, title, sortkey=None, reverse=False):
                 self.request = request
                 self.title = title
@@ -39,14 +40,14 @@ class Search(BrowserView):
 
             def selected(self):
                 return self.request.get('sort_on') == self.sortkey
-                
+
             def url(self):
                 q = {}
                 q.update(self.request.form)
-                if q.has_key('sort_on'):
+                if 'sort_on' in q.keys():
                     del q['sort_on']
-                if q.has_key('sort_order'):
-                    del q['sort_order']                    
+                if 'sort_order' in q.keys():
+                    del q['sort_order']
                 if self.sortkey:
                     q['sort_on'] = self.sortkey
                 if self.reverse:
@@ -55,7 +56,9 @@ class Search(BrowserView):
 
         return(
             sortoption(self.request, 'relevance'),
-            sortoption(self.request, 'date (newest first)', 'Date', reverse=True),
+            sortoption(self.request, 'date (newest first)',
+                                     'Date',
+                                     reverse=True),
             sortoption(self.request, 'aphabetically', 'sortable_title'),
         )
 
@@ -67,7 +70,7 @@ class Search(BrowserView):
             return False
         elif self.request.get('advanced_search', None) == 'True':
             return True
-            
+
     def closeLink(self):
         """URL builder for show/close advanced search filters."""
         query = self.request.get('QUERY_STRING', None)
@@ -75,54 +78,57 @@ class Search(BrowserView):
             return self.request.get('ACTUAL_URL', self.context.absolute_url())
         if 'advanced_search' in query:
             if 'advanced_search=True' in query:
-                query = query.replace('advanced_search=True','')
+                query = query.replace('advanced_search=True', '')
             if 'advanced_search=False' in query:
-                query = query.replace('advanced_search=False','')
-        
-        return self.request.get('ACTUAL_URL', self.context.absolute_url()) + '?' + query
+                query = query.replace('advanced_search=False', '')
+
+        return self.request.get('ACTUAL_URL',
+                                self.context.absolute_url()) + '?' + query
 
     def section(self, url):
-        """ Returns a section in which the object at the passed url is contained. 
-        Section is a first-level folder in Plone root.
+        """ Returns a section in which the object at the passed url is
+        contained. Section is a first-level folder in Plone root.
 
-        For objects that are contained in Plone root object, we just return None
-        as we don't want to display anything on the template.
+        For objects that are contained in Plone root object, we just return
+        None as we don't want to display anything on the template.
 
-        It does that by first removing the portal url part from the object's url, 
-        including the Plone instance id in non-virtual-hostname environments.
-        
-        Then it splits the path with '/', taking the second item which is 
+        It does that by first removing the portal url part from the object's
+        url, including the Plone instance id in non-virtual-hostname
+        environments.
+
+        Then it splits the path with '/', taking the second item which is
         the section's id.
-        
-        Lastly an object is retrieved from the Plone root object that has 
+
+        Lastly an object is retrieved from the Plone root object that has
         the id of the section we are looking for.
-        
+
         Simple caching is put in place to prevent waking up section object for
         objects under the same section.
         """
         # plone root object
         url_tool = getToolByName(self.context, "portal_url")
         portal = url_tool.getPortalObject()
-        
+
         # truncate away http, domain and plone instance id
         path = url.split(url_tool.getPortalPath())[1]
-        
+
         # don't show location for first-level objects
         # -> ['', 'front-page']
         if len(path.split('/')) < 3:
             return None
-        
-        # get sections's id 
+
+        # get sections's id
         section_id = path.split('/')[1]
-        
-        # is this section's Title already stored in sections_cache cache dictionary?
+
+        # is this section's Title already stored in sections_cache cache
+        # dictionary?
         if section_id in self.sections_cache.keys():
             return self.sections_cache[section_id]
-        
+
         # get section object
         section = portal[section_id]
-        
+
         # store title and id in cache
         self.sections_cache[section_id] = section.title
-        
+
         return section.title
