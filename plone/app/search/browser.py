2811d4c334b017fe9b838ead710027ea2a83eb8a
Hanno Schlichting <hanno@hannosch.eu>
2011-06-26 15:13:34 +0000

    Move helper functions to module scope
    svn path=/plone.app.search/trunk/; revision=50746

--- plone/app/search/browser.py
+++ plone/app/search/browser.py
@@ -3,12 +3,48 @@ from Products.CMFCore.utils import getToolByName
 from Products.CMFPlone.browser.navtree import getNavigationRoot
 from Products.CMFPlone.PloneBatch import Batch
 from Products.ZCTextIndex.ParseTree import ParseError
-from ZODB.POSException import ConflictError
 from zope.i18nmessageid import MessageFactory
 from zope.publisher.browser import BrowserView
 from ZTUtils import make_query
 
 _ = MessageFactory('plone')
+
+# We should accept both a simple space, unicode u'\u0020 but also a
+# multi-space, so called 'waji-kankaku', unicode u'\u3000'
+MULTISPACE = u'\u3000'.encode('utf-8')
+
+
+def quotestring(s):
+    return '"%s"' % s
+
+
+def quotequery(s):
+    if not s:
+        return s
+    try:
+        terms = s.split()
+    except Exception:
+        return s
+    tokens = ('OR', 'AND', 'NOT')
+    s_tokens = ('OR', 'AND')
+    check = (0, -1)
+    for idx in check:
+        if terms[idx].upper() in tokens:
+            terms[idx] = quotestring(terms[idx])
+    for idx in range(1, len(terms)):
+        if (terms[idx].upper() in s_tokens and
+            terms[idx-1].upper() in tokens):
+            terms[idx] = quotestring(terms[idx])
+    return ' '.join(terms)
+
+
+def quote_bad_chars(s):
+    # We need to quote parentheses when searching text indices (we use
+    # quote_logic_indexes as the list of text indices)
+    bad_chars = ["(", ")"]
+    for char in bad_chars:
+        s = s.replace(char, quotestring(char))
+    return s
 
 
 class Search(BrowserView):
@@ -58,42 +94,6 @@ class Search(BrowserView):
         indexes = catalog.indexes()
         second_pass = {}
 
-        # See http://dev.plone.org/plone/ticket/9422 for
-        # an explanation of '\u3000'
-        multispace = u'\u3000'.encode('utf-8')
-
-        def quotestring(s):
-            return '"%s"' % s
-
-        def quotequery(s):
-            if not s:
-                return s
-            try:
-                terms = s.split()
-            except ConflictError:
-                raise
-            except:
-                return s
-            tokens = ('OR', 'AND', 'NOT')
-            s_tokens = ('OR', 'AND')
-            check = (0, -1)
-            for idx in check:
-                if terms[idx].upper() in tokens:
-                    terms[idx] = quotestring(terms[idx])
-            for idx in range(1, len(terms)):
-                if (terms[idx].upper() in s_tokens and
-                    terms[idx-1].upper() in tokens):
-                    terms[idx] = quotestring(terms[idx])
-            return ' '.join(terms)
-
-        # We need to quote parentheses when searching text indices (we use
-        # quote_logic_indexes as the list of text indices)
-        def quote_bad_chars(s):
-            bad_chars = ["(", ")"]
-            for char in bad_chars:
-                s = s.replace(char, quotestring(char))
-            return s
-
         def ensureFriendlyTypes(query):
             ploneUtils = getToolByName(context, 'plone_utils')
             portal_type = query.get('portal_type', [])
@@ -119,8 +119,8 @@ class Search(BrowserView):
             if v and k in indexes:
                 if k in quote_logic_indexes:
                     v = quote_bad_chars(v)
-                    if multispace in v:
-                        v = v.replace(multispace, ' ')
+                    if MULTISPACE in v:
+                        v = v.replace(MULTISPACE, ' ')
                     if quote_logic:
                         v = quotequery(v)
                 query[k] = v
