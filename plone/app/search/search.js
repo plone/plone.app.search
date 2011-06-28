d3591b3beacbda8009949a801bb9f3e3b59425d9
Denys Mishunov <denys.mishunov@gmail.com>
2011-06-28 18:25:37 +0000

    taking advantage of the built-in everything-is-object nature of javascript — we, now, have global Search object with the following methods: \n 	- getResults() // makes actual ajax call, gets results and updates the elements \n 	- pushState() // updates URL and pushes the state to browser's history \n 	- popState() // reverts search results to the previous history state (used by browser's Back button) \n 	- updateResults() // combination of getResults() and pushState()
    svn path=/plone.app.search/trunk/; revision=50812

--- plone/app/search/search.js
+++ plone/app/search/search.js
@@ -6,76 +6,131 @@
 
     $(function () {
 
-        var container, data, updateResults, pushState, popped, initialURL;
+        var Search, container, data;
         container = $('#search-results');
 
         $('#search-filter input.searchPage[type="submit"]').hide();
 
-        updateResults = function (data) {
-            var str, struct, st, initData;
-            // We save original query string for the RSS link's update further
-            // down
-            initData = data;
-            $.ajax({
-                url: '@@updated_search',
-                data: data,
-                success: function (data) {
-                    // We don't simply hide() the container, but change it's
-                    // opacity in order for the container keep it's place in the
-                    // elements' flow while we make AJAX call and are getting the
-                    // updated results. This gives us smoother transition from
-                    // one results to another.
-                    container.hide();
-                    container.html(data);
-                    container.fadeIn();
+        Search = {
+            // The globally available object with all the methods to handle
+            // the search results
+            getResults: function (data) {
+                // Build an AJAX request to get results from a query defined
+                // in 'data'. data assumes the string of parameters, not an 
+                // Array.
 
-                    st = $('#updated-search-term').text();
-                    if ($('#search-term').length === 0) {
-                        // Until now we had queries with empty search term. So
-                        // we need a placeholder for the search term in
-                        // result's title.
-                        $('h1.documentFirstHeading').append('<strong id="search-term" />');
-                    }
-                    $('#search-term').text(function () {
-                        str = st;
-                        $('#updated-search-term').remove();
-                        return str;
-                    });
+                var str, struct, st, initData;
+                // We save original query string for the RSS link's update further
+                // down
+                initData = data;
+                $.ajax({
+                    url: '@@updated_search',
+                    data: data,
+                    success: function (data) {
+                        // We don't simply hide() the container, but change it's
+                        // opacity in order for the container keep it's place in the
+                        // elements' flow while we make AJAX call and are getting the
+                        // updated results. This gives us smoother transition from
+                        // one results to another.
+                        container.hide();
+                        container.html(data);
+                        container.fadeIn();
 
-                    $('#search-results-number').text(function () {
-                        str = $('#updated-search-results-number').text();
-                        $('#updated-search-results-number').remove();
-                        return str;
-                    });
-                    $('#search-results-bar #sorting-options').html(
-                        function () {
-                            struct = $('#updated-sorting-options').html();
-                            $('#updated-sorting-options').remove();
-                            return struct;
+                        st = $('#updated-search-term').text();
+                        if ($('#search-term').length === 0) {
+                            // Until now we had queries with empty search term. So
+                            // we need a placeholder for the search term in
+                            // result's title.
+                            $('h1.documentFirstHeading').append('<strong id="search-term" />');
                         }
-                    );
-                    $('#rss-subscription a.link-feed').attr('href', function () {
-                        return portal_url + '/search_rss?' + initData;
-                    });
-                },
-                error: function (req, error) {
-                    return true;
-                }
-            });
-        };
+                        $('#search-term').text(function () {
+                            str = st;
+                            $('#updated-search-term').remove();
+                            return str;
+                        });
 
-        pushState = function (initData) {
-            // Now we need to update the browser's path bar to reflect
-            // the URL we are at now and to push a history state change
-            // in the browser's history. We are using Modernizr
-            // library to check whether browser supports HTML5 History
-            // API natively or it needs a polyfill, that provides
-            // hash-change events to the older browser
-            if (Modernizr.history) {
-                // portal_url is the global JS variable available
-                // everywhere in Plone
-                var url = portal_url + '/@@search?' + initData;
-                history.pushState(null, null, url);
+                        $('#search-results-number').text(function () {
+                            str = $('#updated-search-results-number').text();
+                            $('#updated-search-results-number').remove();
+                            return str;
+                        });
+                        $('#search-results-bar #sorting-options').html(
+                            function () {
+                                struct = $('#updated-sorting-options').html();
+                                $('#updated-sorting-options').remove();
+                                return struct;
+                            }
+                        );
+                        $('#rss-subscription a.link-feed').attr('href', function () {
+                            return portal_url + '/search_rss?' + initData;
+                        });
+                    },
+                    error: function (req, error) {
+                        return true;
+                    }
+                });
+            },
+
+            pushState: function (initData) {
+                // Now we need to update the browser's path bar to reflect
+                // the URL we are at now and to push a history state change
+                // in the browser's history. We are using Modernizr
+                // library to check whether browser supports HTML5 History
+                // API natively or it needs a polyfill, that provides
+                // hash-change events to the older browser
+                if (Modernizr.history) {
+                    // portal_url is the global JS variable available
+                    // everywhere in Plone
+                    var url = portal_url + '/@@search?' + initData;
+                    history.pushState(null, null, url);
+                }
+            },
+
+            popState: function () {
+                var popped, initialURL;
+                // THE HANDLER FOR 'POPSTATE' EVENT IS COPIED FROM PJAX.JS
+                // https://github.com/defunkt/jquery-pjax
+
+                // Used to detect initial (useless) popstate.
+                // If history.state exists, assume browser isn't going to fire initial popstate.
+                popped = ('state' in window.history);
+                initialURL = location.href;
+
+
+                // popstate handler takes care of the back and forward buttons
+                //
+                // No need to wrap 'popstate' event handler for window object with
+                // Modernizr check up since popstate event will contain any data only if
+                // a state has been created with history.pushState() that is wrapped in
+                // Modernizr checkup above.
+                $(window).bind('popstate', function (event) {
+                    var initialPop, str;
+                    // Ignore inital popstate that some browsers fire on page load
+                    initialPop = !popped && location.href === initialURL;
+                    popped = true;
+                    if (initialPop) {
+                        return;
+                    }
+
+                    data = location.search.split('?')[1];
+                    // We need to make sure we update the search field with the search
+                    // term from previous query when going back in history
+                    str = data.match(/SearchableText=[^&]*/)[0];
+                    str = str.replace(/\+/g, ' '); // we remove '+' used between words
+                    // in search queries.
+
+                    // Now we have something like 'SearchableText=test' in str
+                    // variable. So, we know when the actual search term begins at
+                    // position 15 in that string.
+                    $('#search-field input[name="SearchableText"], input#searchGadget').val(str.substr(15, str.length));
+
+                    Search.updateResults(data);
+                });
+            },
+
+            updateResults: function (data) {
+                Search.getResults(data);
+                Search.pushState(data);
             }
         };
 
@@ -89,15 +144,13 @@
             query = location.search.replace(/SearchableText=[^&]*/, 'SearchableText=' + st);
             data = query.split('?')[1];
             $(container).fadeOut('fast');
-            updateResults(data);
-            pushState(data);
+            Search.updateResults(data);
             e.preventDefault();
         });
         $('form.searchPage').submit(function (e) {
             data = $('form.searchPage').serialize();
             $(container).fadeOut('fast');
-            updateResults(data);
-            pushState(data);
+            Search.updateResults(data);
             e.preventDefault();
         });
 
@@ -106,7 +159,6 @@
         $('#search-field input[name="SearchableText"]').keyup(function () {
             $('input#searchGadget').val($(this).val());
         });
-
 
         // When we click any option in the Filter menu, we need to prevent the
         // menu from being closed as it is dictaded by dropdown.js for all
@@ -121,8 +173,7 @@
             function (e) {
                 data = $('form.searchPage').serialize();
                 $(container).fadeOut('fast');
-                updateResults(data);
-                pushState(data);
+                Search.updateResults(data);
                 e.preventDefault();
             }
         );
@@ -139,8 +190,7 @@
                 $("form.searchPage input[name='sort_on']").val('');
             }
             data = this.search.split('?')[1];
-            updateResults(data);
-            pushState(data);
+            Search.updateResults(data);
             e.preventDefault();
         });
 
@@ -148,48 +198,8 @@
         // well.
         $('.listingBar a').live('click', function (e) {
             data = this.search.split('?')[1];
-            updateResults(data);
-            pushState(data);
+            Search.updateResults(data);
             e.preventDefault();
-        });
-
-        // THE HANDLER FOR 'POPSTATE' EVENT IS COPIED FROM PJAX.JS
-        // https://github.com/defunkt/jquery-pjax
-
-        // Used to detect initial (useless) popstate.
-        // If history.state exists, assume browser isn't going to fire initial popstate.
-        popped = ('state' in window.history);
-        initialURL = location.href;
-
-
-        // popstate handler takes care of the back and forward buttons
-        //
-        // No need to wrap 'popstate' event handler for window object with
-        // Modernizr check up since popstate event will contain any data only if
-        // a state has been created with history.pushState() that is wrapped in
-        // Modernizr checkup above.
-        $(window).bind('popstate', function (event) {
-            var initialPop, str;
-            // Ignore inital popstate that some browsers fire on page load
-            initialPop = !popped && location.href === initialURL;
-            popped = true;
-            if (initialPop) {
-                return;
-            }
-
-            data = location.search.split('?')[1];
-            // We need to make sure we update the search field with the search
-            // term from previous query when going back in history
-            str = data.match(/SearchableText=[^&]*/)[0];
-            str = str.replace(/\+/g, ' '); // we remove '+' used between words
-            // in search queries.
-
-            // Now we have something like 'SearchableText=test' in str
-            // variable. So, we know when the actual search term begins at
-            // position 15 in that string.
-            $('#search-field input[name="SearchableText"], input#searchGadget').val(str.substr(15, str.length));
-
-            updateResults(data);
         });
     });
 
