5b384ae98f105bb562185b6abd874ea66e8518c2
Denys Mishunov <denys.mishunov@gmail.com>
2011-07-08 20:30:44 +0000

    re-organization of the search.js. We don't provide any global static function, but instead we try to provide global method for pulling search results into a 'container', defined by the selector. Not sure even this will survive though… References #9352
    svn path=/plone.app.search/trunk/; revision=51071

--- plone/app/search/search.js
+++ plone/app/search/search.js
@@ -1,206 +1,191 @@
 /* The following line defines global variables defined elsewhere. */
 /*globals jQuery, portal_url, Modernizr, alert, history, window, location*/
 
+jQuery(function ($) {
 
-(function ($) {
+    var query, pushState, popState,
+        Search = {},
+        $default_res_container = $('#search-results');
 
-    $(function () {
+    // The globally available method to pull the search results for the
+    // 'query' into the element, on which the method is invoked
+    $.fn.pullSearchResults = function (query) {
+        return this.each(function () {
+            var $container = $(this);
+            $.get(
+                '@@updated_search',
+                query,
+                function (data) {
+                    // Before assigning any variable we need to make sure we
+                    // have the returned data available (returned somewhere to
+                    // the DOM tree). Otherwise we will not be able to select
+                    // elements from the returned HTML.
+                    $container.hide();
+                    $container.html(data);
+                    $container.fadeIn();
 
-        var Search, container, data;
-        container = $('#search-results');
+                    var data_res = $('#search-results'),
+                        data_search_term = $('#updated-search-term').text(),
+                        data_res_number = $('#updated-search-results-number').text(),
+                        data_sorting_opt = $('#updated-sorting-options').html();
 
-        $('#search-filter input.searchPage[type="submit"]').hide();
+                    if ($('#search-term').length === 0) {
+                        // Until now we had queries with empty search term. So
+                        // we need a placeholder for the search term in
+                        // result's title.
+                        $('h1.documentFirstHeading').append('<strong id="search-term" />');
+                    }
+                    $('#search-term').text(function () {
+                        $('#updated-search-term').remove();
+                        return data_search_term;
+                    });
 
-        Search = {
-            // The globally available object with all the methods to handle
-            // the search results
-            getResults: function (data) {
-                // Build an AJAX request to get results from a query defined
-                // in 'data'. data assumes the string of parameters, not an 
-                // Array.
-
-                var str, struct, st, initData;
-                // We save original query string for the RSS link's update further
-                // down
-                initData = data;
-                $.ajax({
-                    url: '@@updated_search',
-                    data: data,
-                    success: function (data) {
-                        // We don't simply hide() the container, but change it's
-                        // opacity in order for the container keep it's place in the
-                        // elements' flow while we make AJAX call and are getting the
-                        // updated results. This gives us smoother transition from
-                        // one results to another.
-                        container.hide();
-                        container.html(data);
-                        container.fadeIn();
-
-                        st = $('#updated-search-term').text();
-                        if ($('#search-term').length === 0) {
-                            // Until now we had queries with empty search term. So
-                            // we need a placeholder for the search term in
-                            // result's title.
-                            $('h1.documentFirstHeading').append('<strong id="search-term" />');
+                    $('#search-results-number').text(function () {
+                        $('#updated-search-results-number').remove();
+                        return data_res_number;
+                    });
+                    $('#search-results-bar #sorting-options').html(
+                        function () {
+                            $('#updated-sorting-options').remove();
+                            return data_sorting_opt;
                         }
-                        $('#search-term').text(function () {
-                            str = st;
-                            $('#updated-search-term').remove();
-                            return str;
-                        });
-
-                        $('#search-results-number').text(function () {
-                            str = $('#updated-search-results-number').text();
-                            $('#updated-search-results-number').remove();
-                            return str;
-                        });
-                        $('#search-results-bar #sorting-options').html(
-                            function () {
-                                struct = $('#updated-sorting-options').html();
-                                $('#updated-sorting-options').remove();
-                                return struct;
-                            }
-                        );
-                        $('#rss-subscription a.link-feed').attr('href', function () {
-                            return portal_url + '/search_rss?' + initData;
-                        });
-                    },
-                    error: function (req, error) {
-                        return true;
-                    }
+                    );
+                    $('#rss-subscription a.link-feed').attr('href', function () {
+                        return portal_url + '/search_rss?' + query;
+                    });
                 });
-            },
+        });
+    };
 
-            pushState: function (initData) {
-                // Now we need to update the browser's path bar to reflect
-                // the URL we are at now and to push a history state change
-                // in the browser's history. We are using Modernizr
-                // library to check whether browser supports HTML5 History
-                // API natively or it needs a polyfill, that provides
-                // hash-change events to the older browser
-                if (Modernizr.history) {
-                    // portal_url is the global JS variable available
-                    // everywhere in Plone
-                    var url = portal_url + '/@@search?' + initData;
-                    history.pushState(null, null, url);
-                }
-            },
+    pushState = function (query) {
+        // Now we need to update the browser's path bar to reflect
+        // the URL we are at now and to push a history state change
+        // in the browser's history. We are using Modernizr
+        // library to check whether browser supports HTML5 History
+        // API natively or it needs a polyfill, that provides
+        // hash-change events to the older browser
+        if (Modernizr.history) {
+            // portal_url is the global JS variable available
+            // everywhere in Plone
+            var url = portal_url + '/@@search?' + query;
+            history.pushState(null, null, url);
+        }
+    };
 
-            popState: function () {
-                var popped, initialURL;
-                // THE HANDLER FOR 'POPSTATE' EVENT IS COPIED FROM PJAX.JS
-                // https://github.com/defunkt/jquery-pjax
+    popState = function () {
+        var popped, initialURL;
+        // THE HANDLER FOR 'POPSTATE' EVENT IS COPIED FROM PJAX.JS
+        // https://github.com/defunkt/jquery-pjax
 
-                // Used to detect initial (useless) popstate.
-                // If history.state exists, assume browser isn't going to fire initial popstate.
-                popped = ('state' in window.history);
-                initialURL = location.href;
+        // Used to detect initial (useless) popstate.
+        // If history.state exists, assume browser isn't going to fire initial popstate.
+        popped = ('state' in window.history);
+        initialURL = location.href;
 
 
-                // popstate handler takes care of the back and forward buttons
-                //
-                // No need to wrap 'popstate' event handler for window object with
-                // Modernizr check up since popstate event will contain any data only if
-                // a state has been created with history.pushState() that is wrapped in
-                // Modernizr checkup above.
-                $(window).bind('popstate', function (event) {
-                    var initialPop, str;
-                    // Ignore inital popstate that some browsers fire on page load
-                    initialPop = !popped && location.href === initialURL;
-                    popped = true;
-                    if (initialPop) {
-                        return;
-                    }
-
-                    data = location.search.split('?')[1];
-                    // We need to make sure we update the search field with the search
-                    // term from previous query when going back in history
-                    str = data.match(/SearchableText=[^&]*/)[0];
-                    str = str.replace(/\+/g, ' '); // we remove '+' used between words
-                    // in search queries.
-
-                    // Now we have something like 'SearchableText=test' in str
-                    // variable. So, we know when the actual search term begins at
-                    // position 15 in that string.
-                    $('#search-field input[name="SearchableText"], input#searchGadget').val(str.substr(15, str.length));
-
-                    Search.updateResults(data);
-                });
-            },
-
-            updateResults: function (data) {
-                Search.getResults(data);
-                Search.pushState(data);
+        // popstate handler takes care of the back and forward buttons
+        //
+        // No need to wrap 'popstate' event handler for window object with
+        // Modernizr check up since popstate event will contain any data only if
+        // a state has been created with history.pushState() that is wrapped in
+        // Modernizr checkup above.
+        $(window).bind('popstate', function (event) {
+            var initialPop, str;
+            // Ignore inital popstate that some browsers fire on page load
+            initialPop = !popped && location.href === initialURL;
+            popped = true;
+            if (initialPop) {
+                return;
             }
-        };
 
-        // We don't submit the whole form with all the fields when only the
-        // search term is being changed. We just alter the current URL to
-        // substitue the search term and make a new ajax call to get updated
-        // results
-        $('#searchPage input.searchPage').click(function (e) {
-            var st, query;
-            st = $('#search-field input.searchPage').val();
-            query = location.search.replace(/SearchableText=[^&]*/, 'SearchableText=' + st);
-            data = query.split('?')[1];
-            $(container).fadeOut('fast');
-            Search.updateResults(data);
-            e.preventDefault();
-        });
-        $('form.searchPage').submit(function (e) {
-            data = $('form.searchPage').serialize();
-            $(container).fadeOut('fast');
-            Search.updateResults(data);
-            e.preventDefault();
-        });
+            query = location.search.split('?')[1];
+            // We need to make sure we update the search field with the search
+            // term from previous query when going back in history
+            str = query.match(/SearchableText=[^&]*/)[0];
+            str = str.replace(/\+/g, ' '); // we remove '+' used between words
+            // in search queries.
 
-        // We need to update the site-wide search field (at the top right in
-        // stock Plone) when the main search field is updated
-        $('#search-field input[name="SearchableText"]').keyup(function () {
-            $('input#searchGadget').val($(this).val());
-        });
+            // Now we have something like 'SearchableText=test' in str
+            // variable. So, we know when the actual search term begins at
+            // position 15 in that string.
+            $('#search-field input[name="SearchableText"], input#searchGadget').val(str.substr(15, str.length));
 
-        // When we click any option in the Filter menu, we need to prevent the
-        // menu from being closed as it is dictaded by dropdown.js for all
-        // dl.actionMenu > dd.actionMenuContent
-        $('#search-results-bar dl.actionMenu > dd.actionMenuContent').click(function (e) {
-            e.stopImmediatePropagation();
+            Search.updateResults(query);
         });
+    };
 
-        // Now we can handle the actual menu options and update the search
-        // results after any of them has been chosen.
-        $('#search-filter input, #search-filter select').not('input#pt_toggle').live('change',
-            function (e) {
-                data = $('form.searchPage').serialize();
-                $(container).fadeOut('fast');
-                Search.updateResults(data);
-                e.preventDefault();
-            }
-        );
+    $('#search-filter input.searchPage[type="submit"]').hide();
 
-        // Since we replace the whole sorting options with HTML, coming in
-        // AJAX response, we should bind the click event with live() in order
-        // for this to keep working with the HTML elements, coming from AJAX
-        // respons
-        $('#sorting-options a').live('click', function (e) {
-            if ($(this).attr('data-sort')) {
-                $("form.searchPage input[name='sort_on']").val($(this).attr('data-sort'));
-            }
-            else {
-                $("form.searchPage input[name='sort_on']").val('');
-            }
-            data = this.search.split('?')[1];
-            Search.updateResults(data);
-            e.preventDefault();
-        });
-
-        // Handle clicks in the batch navigation bar. Load those with Ajax as
-        // well.
-        $('.listingBar a').live('click', function (e) {
-            data = this.search.split('?')[1];
-            Search.updateResults(data);
-            e.preventDefault();
-        });
+    // We don't submit the whole form with all the fields when only the
+    // search term is being changed. We just alter the current URL to
+    // substitue the search term and make a new ajax call to get updated
+    // results
+    $('#searchPage input.searchPage').click(function (e) {
+        var st, query;
+        st = $('#search-field input[name="SearchableText"]').val();
+        query = location.search.replace(/SearchableText=[^&]*/, 'SearchableText=' + st);
+        query = query.split('?')[1];
+        $default_res_container.fadeOut('fast');
+        $default_res_container.pullSearchResults(query);
+        pushState(query);
+        e.preventDefault();
+    });
+    $('form.searchPage').submit(function (e) {
+        query = $('form.searchPage').serialize();
+        $default_res_container.fadeOut('fast');
+        $default_res_container.pullSearchResults(query);
+        pushState(query);
+        e.preventDefault();
     });
 
-}(jQuery));
+    // We need to update the site-wide search field (at the top right in
+    // stock Plone) when the main search field is updated
+    $('#search-field input[name="SearchableText"]').keyup(function () {
+        $('input#searchGadget').val($(this).val());
+    });
+
+    // When we click any option in the Filter menu, we need to prevent the
+    // menu from being closed as it is dictaded by dropdown.js for all
+    // dl.actionMenu > dd.actionMenuContent
+    $('#search-results-bar dl.actionMenu > dd.actionMenuContent').click(function (e) {
+        e.stopImmediatePropagation();
+    });
+
+    // Now we can handle the actual menu options and update the search
+    // results after any of them has been chosen.
+    $('#search-filter input, #search-filter select').not('input#pt_toggle').live('change',
+        function (e) {
+            query = $('form.searchPage').serialize();
+            $default_res_container.fadeOut('fast');
+            $default_res_container.pullSearchResults(query);
+            pushState(query);
+            e.preventDefault();
+        }
+    );
+
+    // Since we replace the whole sorting options with HTML, coming in
+    // AJAX response, we should bind the click event with live() in order
+    // for this to keep working with the HTML elements, coming from AJAX
+    // respons
+    $('#sorting-options a').live('click', function (e) {
+        if ($(this).attr('data-sort')) {
+            $("form.searchPage input[name='sort_on']").val($(this).attr('data-sort'));
+        }
+        else {
+            $("form.searchPage input[name='sort_on']").val('');
+        }
+        query = this.search.split('?')[1];
+        $default_res_container.pullSearchResults(query);
+        pushState(query);
+        e.preventDefault();
+    });
+
+    // Handle clicks in the batch navigation bar. Load those with Ajax as
+    // well.
+    $('.listingBar a').live('click', function (e) {
+        query = this.search.split('?')[1];
+        $default_res_container.pullSearchResults(query);
+        pushState(query);
+        e.preventDefault();
+    });
+});
