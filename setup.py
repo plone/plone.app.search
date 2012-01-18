5544b40babd110759ee0a93f273dd46d1ab9560e
JeanMichel FRANCOIS <toutpt@gmail.com>
2012-01-18 15:12:49 +0100

    add modernizr deps

--- setup.py
+++ setup.py
@@ -31,6 +31,7 @@ setup(name='plone.app.search',
       install_requires=[
           'setuptools',
           'plone.app.contentlisting',
+          'plone.app.modernizr',
           # -*- Extra requirements: -*-
       ],
       tests_require=tests_require,
