5ba0c2044df4f87a86a9c943a66f2e4caee3bb00
Sean Upton <sdupton@gmail.com>
2012-08-21 12:23:57 -0600

    Search.filter_query() checks for any valid indexes specified in request to prevent empty catalog searches, instead of limiting searches to require either SearchableText or Subject query.

--- plone/app/search/tests/base.py
+++ plone/app/search/tests/base.py
@@ -1,6 +1,12 @@
+import sys
 import unittest2 as unittest
 
 from zope.configuration import xmlconfig
+from zope.interface import alsoProvides
+from zope.publisher.browser import setDefaultSkin
+from z3c.form.interfaces import IFormLayer
+from ZPublisher.HTTPResponse import HTTPResponse
+from ZPublisher.HTTPRequest import HTTPRequest
 
 from plone.app.testing import PloneSandboxLayer
 from plone.app.testing import applyProfile
@@ -10,6 +16,22 @@ from plone.app.testing import TEST_USER_NAME, TEST_USER_ID
 from plone.app.testing import login
 from plone.app.testing import setRoles
 from plone.app.testing.selenium_layers import SELENIUM_PLONE_FUNCTIONAL_TESTING
+
+
+def test_request():
+    """ 
+    make request suitable for browser views and Zope2 security.
+    """
+    response = HTTPResponse(stdout=sys.stdout)
+    request = HTTPRequest(sys.stdin,
+                      {'SERVER_NAME'    : 'localhost',
+                       'SERVER_PORT'    : '80',
+                       'REQUEST_METHOD' : 'GET', },
+                      response)
+    request['ACTUAL_URL'] = 'http://nohost/plone'
+    setDefaultSkin(request)
+    alsoProvides(request, IFormLayer) #suitable for testing z3c.form views
+    return request
 
 
 class SearchLayer(PloneSandboxLayer):
