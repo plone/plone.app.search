424cb5aefb9f0eee87c807d9cc7aaa6facd15f48
maartenkling <maarten@fourdigits.nl>
2012-12-03 15:05:16 +0100

    remove console info line

--- plone/app/search/search.js
+++ plone/app/search/search.js
@@ -175,7 +175,6 @@ jQuery(function ($) {
             if ($('input[name="portal_type:list"]:checked').length > 0) {
                 query = $('form.searchPage').serialize();
             }
-            console.info(query);
             $default_res_container.pullSearchResults(query);
             pushState(query);
             e.preventDefault();
