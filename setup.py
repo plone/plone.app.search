70b4d4d93a02bb975d6c559496234132b9d485e3
Eric Steele <EricSteele47@gmail.com>
2012-12-14 22:47:15 -0500

    Preparing release 1.0.7

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '1.0.7.dev0'
+version = '1.0.7'
 
 tests_require = ['collective.testcaselayer',
                  'plone.app.testing',
